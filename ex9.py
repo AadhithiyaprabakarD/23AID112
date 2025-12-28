e29d79405c7d26a4dfd80adffda143c436e86661
AadhithiyaprabakarD <aadhithiyaprabakard@gmail.com>
2025-12-28 20:32:43 +0530

    Delete ex9.py

--- ex9.py
+++
@@ -1,9 +0,0 @@
-# Level 2 Question 9
-
-score1 = int(input("Enter your first exam score : "))
-score2 = int(input("Enter your second exam score : "))
-
-if(score1 and score2 >= 50 ):
-    print("You passed!")
-else:
-    print("You need to retake some exams")
